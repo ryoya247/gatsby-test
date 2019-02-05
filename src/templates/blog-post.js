ee4af59cd4e5bee24a4f52857b50ad59aaef2167
ryoya247 <chan1no.hello.16@gmail.com>
2019-02-05 17:23:17 +0900

    remove blogs

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,7 +11,7 @@ export const BlogPostTemplate = ({
   contentComponent,
   description,
   tags,
-  title,
+  pageTitle,
   helmet,
 }) => {
   const PostContent = contentComponent || Content
@@ -23,7 +23,7 @@ export const BlogPostTemplate = ({
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
+              {pageTitle}
             </h1>
             <p>{description}</p>
             <PostContent content={content} />
@@ -50,7 +50,7 @@ BlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
-  title: PropTypes.string,
+  pageTitle: PropTypes.string,
   helmet: PropTypes.object,
 }
 
@@ -67,12 +67,12 @@ const BlogPost = ({ data }) => {
           <Helmet
             titleTemplate="%s | Blog"
           >
-            <title>{`${post.frontmatter.title}`}</title>
+            <title>{`${post.frontmatter.pageTitle}`}</title>
             <meta name="description" content={`${post.frontmatter.description}`} />
           </Helmet>
         }
         tags={post.frontmatter.tags}
-        title={post.frontmatter.title}
+        pageTitle={post.frontmatter.pageTitle}
       />
     </Layout>
   )
@@ -93,7 +93,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
-        title
+        pageTitle
         description
         tags
       }
